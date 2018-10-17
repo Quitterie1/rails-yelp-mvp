12241b373240c5a71d1fc67e372e50260a8a4071
Quitterie Philipon <quitterie.philipon@edhec.com>
2018-10-17 16:35:29 +0200

    exercice 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:  'italian'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:  'italian'
+  },
+  {
+    name:         'BigMamma',
+    address:      '58B Shoreditch High St, London E1 6PQ',
+    category:  'italian'
+  },
+  {
+    name:         'french restaurant',
+    address:      '59 rue de france, London E1 6PQ',
+    category:  'french'
+  },
+  {
+    name:         'Belgian Restaurant',
+    address:      '56 rue de Belgique, London E1 6PQ',
+    category:  'belgian'
+  }
+]
+Restaurant.create!(restaurants_attributes)
